63387cde84447e579bccfaac0473a4700bb7b84c
ElvinLWH <elvinlawenghoe1@gmail.com>
2023-11-22 21:27:26 +0800

    Added return values for pytest

--- bmi.py
+++ bmi.py
@@ -8,10 +8,13 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5 :
         print("Weight Class = Under Weight")
+        return -1
     elif 18.5 <= bmi <= 25.0 :
         print("Weight Class = Normal Weight")
+        return 0
     else :
         print("Weight Class = Over Weight")
+        return 1
 
 
 calculate_bmi(weight=45, height=1.73)
